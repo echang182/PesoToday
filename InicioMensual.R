f590050bf70444637fee40ac1afb4caa256fe49d
Eloy Chang <spencer.chang.182@gmail.com>
2018-02-04 15:03:45 -0300

    Archivo de inicio de variables

--- InicioMensual.R
+++ InicioMensual.R
@@ -5,3 +5,23 @@ Mensual<- data.frame(
     Limite=c(8000,4000,2400,900,550,1650,12000,2500,0),
     Diferencia=c(8000,4000,2400,900,550,1650,12000,2500,0)
 )
+
+Ingreso<- data.frame(
+    Fecha=character(),
+    Categoria=character(),
+    SubCategoria=character(),
+    Monto=numeric()
+)
+
+Ingreso$Fecha<- as.Date(as.character(Ingreso$Fecha))
+Ingreso$Categoria<- as.character(Ingreso$Categoria)
+Ingreso$SubCategoria<- as.character(Ingreso$SubCategoria)
+
+Balance<- data.frame(
+    Fecha=Sys.Date()-4,
+    Ingreso=0,
+    Egreso=0,
+    Saldo=0
+)
+
+
